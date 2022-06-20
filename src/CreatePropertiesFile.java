57658daa4e460e2f78ec63a30fd72a83b39ad315
afiffifa <mafifny@gmail.com>
2022-06-20 17:03:50 -0400

    FileOutput

--- src/CreatePropertiesFile.java
+++ src/CreatePropertiesFile.java
@@ -1,10 +1,8 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class createPropertiesFile {
+public class CreatePropertiesFile {
     public static void main(String[] args) throws IOException {
         Properties prop = new Properties();
         prop.put("Year", "2016");
